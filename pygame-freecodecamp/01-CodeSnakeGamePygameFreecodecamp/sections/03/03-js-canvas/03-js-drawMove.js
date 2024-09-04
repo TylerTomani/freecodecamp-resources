1984cb033436cb589139c6e40afc2dea96db4be0
Tyler Tomani <ttomani@gmail.com>
2024-09-04 15:34:08 -0400

    main

--- pygame-freecodecamp/01-CodeSnakeGamePygameFreecodecamp/sections/03/03-js-canvas/03-js-drawMove.js
+++ pygame-freecodecamp/01-CodeSnakeGamePygameFreecodecamp/sections/03/03-js-canvas/03-js-drawMove.js
@@ -1,1 +1,11 @@
-const canvas = document.querySelector('selectors')
+export function drawMove(){
+
+    
+    const canvas = document.querySelector('canvas')
+    const c = canvas.getContext('2d')
+    console.log(canvas)
+    c.fillStyle = 'black'
+    c.fill(0,0,innerWidth,innerWidth)
+    console.log('lkjd')
+}
+// drawMove()
