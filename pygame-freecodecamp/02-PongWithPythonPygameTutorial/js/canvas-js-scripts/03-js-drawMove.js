7904c7d71d501c9433471c47c0e2e36b6feaa866
Tyler Tomani <ttomani@gmail.com>
2024-09-05 14:40:07 -0400

    main

--- pygame-freecodecamp/02-PongWithPythonPygameTutorial/js/canvas-js-scripts/03-js-drawMove.js
+++ pygame-freecodecamp/02-PongWithPythonPygameTutorial/js/canvas-js-scripts/03-js-drawMove.js
@@ -1,11 +1,17 @@
 (function(){
     const canvas = document.querySelector('canvas')
     const c = canvas.getContext('2d')
+    canvas.width = innerWidth
+    canvas.height = innerHeight * .7
     let xBlock = 10
     let yBlock = 10
     let heightWidth = 20
-    const block = new Image();
-    block.src = './js/canvas-js-scripts/img/block.jpg';
+    const blockImg = new Image()
+    blockImg.src = './js/canvas-js-scripts/img/block.jpg';
+    let velocity = 1
+    
+
+    let speedOfBlock = .025
     const keys = {
         down :{
             pressed: false
@@ -21,27 +27,49 @@
         },
         
     }
+    class Block {
+        constructor(x,y,image){
+            this.x = x
+            this.y = y
+            this.image = image
+            this.width = 30
+            this.velocity = {
+                x: 0,
+                y: 0
+            }
+        }
+        draw(){
+            c.drawImage(this.image, this.x, this.y, this.width, this.width)
+        }
+        update(){
+            this.draw()
+            this.x += this.velocity.x  
+            this.y += this.velocity.y 
+            // this.y += this.velocity
+        }
+    }
+    const block = new Block(10 , 10 , blockImg)
+   
     function animate(){
         requestAnimationFrame(animate)
         c.fillStyle = 'rgb(255,255,255)'
-        // c.fillRect(0,0,canvas.width,canvas.height)
-        c.drawImage(block, xBlock, yBlock, heightWidth, heightWidth)
-        if(keys.down.pressed){
-            yBlock += heightWidth * .1
+        c.fillRect(0,0,canvas.width,canvas.height)
+        block.update()    
+        if(keys.down.pressed && !keys.up.pressed && (block.y + block.velocity.y + block.width) < canvas.height
+        && block.y + block.width > 0){
+            block.velocity.y += velocity * speedOfBlock
+        } else if (keys.up.pressed && !keys.down.pressed){
+            block.velocity.y -= velocity * speedOfBlock
+        } else {
+            // block.velocity.y = 0
         }
-        if(keys.right.pressed){
-            xBlock += heightWidth * .1
+        if (keys.right.pressed && !keys.left.pressed){
+            block.velocity.x += speed * speedOfBlock
+        } else if (keys.left.pressed && !keys.right.pressed ){
+            block.velocity.x -= speed * speedOfBlock
+        } else {
+            // block.velocity.x = 0
         }
-        if(keys.up.pressed){
-            yBlock -= heightWidth * .1
-        }
-        if(keys.left.pressed){
-            xBlock -= heightWidth * .1
-        }
-
-
-
-        
 
     }
     animate()
@@ -64,7 +92,6 @@
     })
     addEventListener('keydown', e => {
         let key = e.keyCode
-        console.log(key)
         switch(key){
             case 40:
                 e.preventDefault()
@@ -83,5 +110,25 @@
                 keys.left.pressed = true
                 break
         }
+    })
+    let speed = 2
+    canvas.addEventListener('click', e => {
+        const rect = canvas.getBoundingClientRect()
+        const xClick = e.clientX - rect.left
+        const yClick = e.clientY - rect.top
+        console.clear()
+        if(xClick > block.x && (block.x < canvas.width && block.x + block.width > 0)){
+            block.velocity.x += speed
+        } 
+        if(xClick < block.x){
+            block.velocity.x -= speed
+        }
+        if(yClick > block.y){
+            block.velocity.y += speed
+        } 
+        if(yClick < block.y){
+            block.velocity.y -= speed
+        }
+
     })
 }())
