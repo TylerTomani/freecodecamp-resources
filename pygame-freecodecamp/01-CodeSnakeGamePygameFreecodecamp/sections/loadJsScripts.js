d329b613c5a1df8b0ffdbdae6e82252556af6cb4
tt <tt@tts-MacBook-Pro.lan>
2024-09-14 06:05:34 -0400

    main

--- pygame-freecodecamp/01-CodeSnakeGamePygameFreecodecamp/sections/loadJsScripts.js
+++ pygame-freecodecamp/01-CodeSnakeGamePygameFreecodecamp/sections/loadJsScripts.js
@@ -4,7 +4,8 @@ export function injectJsScripts(){
     
 
     const jsCanvasScriptContainer = document.querySelector('#jsCanvasScriptContainer')
-    const jsParenCode = jsCanvasScriptContainer.querySelector(' #mainCode')
+    const jsParenCode = jsCanvasScriptContainer.querySelector('#mainCode')
+    const mainCodeJs = jsCanvasScriptContainer.querySelector('.mainCodeJS')
     const tempScript = document.getElementById('tempScript')
     const titleJsScript = document.getElementById('titleJsScript')
     if(titleJsScript){
@@ -16,22 +17,44 @@ export function injectJsScripts(){
             fetch(injectScript)
                 .then(response => response.text())
                 .then(data => {
-                    jsParenCode.innerHTML = data
-                    // // Remove old script elements if they exist
-                    const tempDiv = document.createElement('div');
-                    tempDiv.innerHTML = data;
-                    
-                    // // Create and append new script element
-                    // document.removeChild(tempScript)                                
-                    const scriptContent = jsParenCode.textContent;
-    
-                    const tempScript = document.createElement('script');
-                    tempScript.setAttribute('id', 'tempScript')
-                    tempScript.type = 'text/javascript';
-                    tempScript.textContent = scriptContent;
-                    tempScript.setAttribute('data-dynamic', 'true'); // Optional: mark as dynamic to easily remove later
-                    if(tempScript && scriptContent){
-                        document.body.appendChild(tempScript);
+                    if(mainCodeJs){
+                        jsParenCode.innerHTML = data
+                        // // Remove old script elements if they exist
+                        const tempDiv = document.createElement('div');
+                        tempDiv.innerHTML = data;
+                        
+                        // // Create and append new script element
+                        // document.removeChild(tempScript)                                
+                        const scriptContent = jsParenCode.textContent;
+        
+                        const tempScript = document.createElement('script');
+                        
+                        tempScript.setAttribute('id', 'tempScript')
+                        tempScript.type = 'text/javascript';
+                        tempScript.textContent = scriptContent;
+                        tempScript.setAttribute('data-dynamic', 'true'); // Optional: mark as dynamic to easily remove later
+                        if(tempScript && scriptContent){
+                            document.body.appendChild(tempScript);
+                        }
+                    } else {
+                        // jsParenCode.innerHTML = data
+                        // // // Remove old script elements if they exist
+                        // const tempDiv = document.createElement('div');
+                        // tempDiv.innerHTML = data;
+                        
+                        // // Create and append new script element
+                        // document.removeChild(tempScript)                                
+                        const scriptContent = data.textContent;
+                        console.log(data)
+                        const tempScript = document.createElement('script');
+                        
+                        tempScript.setAttribute('id', 'tempScript')
+                        tempScript.type = 'text/javascript';
+                        tempScript.textContent = data;
+                        tempScript.setAttribute('data-dynamic', 'true'); // Optional: mark as dynamic to easily remove later
+                        if(tempScript && data){
+                            document.body.appendChild(tempScript);
+                        }
                     }
                 })
                 .catch(error => console.error('Error loading script:', error));
